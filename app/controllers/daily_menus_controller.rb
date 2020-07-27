d5b14dde79bbe54f9e0300205b45baede3340831
Akshay Nanda <akshayn@sfu.ca>
2020-07-26 22:02:51 -0700

    view : new dailymenu added

--- app/controllers/daily_menus_controller.rb
+++ app/controllers/daily_menus_controller.rb
@@ -1,6 +1,6 @@
 class DailyMenusController < ApplicationController
   before_action :authenticate_user!
-  before_action :authorize!, only: [ :create, :show, :update, :destroy]
+  before_action :authorize!, only: [ :show, :update, :destroy]
   
   def index
     @dailyMenus = DailyMenu.all
@@ -17,12 +17,27 @@ class DailyMenusController < ApplicationController
     #   @course.push(bute.course.name)
     #   @categor.push(bute.course.categori.name)
     # end
-   
-    
   end
   
+  def new 
+    categor = Categori.all
+    
+    courses = Course.all
+    
+    @category = []
+    @course = []
+    
+    categor.each do |cat|
+      @category.push(cat.name)
+    end 
+    courses.each do |cor|
+      @course.push(cor.name)
+    end 
+
+  end
   
   def create
+    puts "hello"
   end
 
   
@@ -36,7 +51,7 @@ class DailyMenusController < ApplicationController
   end
   private
   def authorize! 
-    @dailyMenu = DailyMenu.find(params[ :id])
+    @dailyMenu = DailyMenu.find(params[ :id]) 
     redirect_to root_path, alert: 'Not Authorized' unless can?(:crud, @dailyMenu)
   end
   
